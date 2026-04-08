3ac389a15ffb2c434315286114a0bd4c01ff2628
Surya <135094097+Surya-2011-1981@users.noreply.github.com>
2026-04-08 14:15:38 +0530

    Delete netflix-clone directory

--- netflix-clone/src/pages/index.js
+++
@@ -1,7 +0,0 @@
-// Page exports
-export { HomePage } from './HomePage';
-export { CategoryPage } from './CategoryPage';
-export { SearchPage } from './SearchPage';
-export { ProfilePage } from './ProfilePage';
-export { SignInPage } from './SignInPage';
-export { SignUpPage } from './SignUpPage';
